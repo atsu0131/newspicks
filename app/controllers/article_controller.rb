30e1d7f894e6a1fcebd92fec6d87931042a0d5b0
a <>
2018-05-05 22:24:09 +0900

    カラム作り直し

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -3,7 +3,15 @@ class ArticleController < ApplicationController
   def index
   end
 
+  def create
+  end
 
   def authentication
   end
+
+    private
+    def move_to_index
+      redirect_to action: :index
+      # indexアクションを強制的に実行する
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'article#index'
  get 'article', to: 'article#index'
 end
