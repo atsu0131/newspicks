2fdcf8147c0408169803d6b31a098bda9e77be85
a <>
2018-05-09 15:06:15 +0900

    controller修正

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,17 +1,16 @@
 class ArticleController < ApplicationController
-  before_action :authenticate_user!, only: :authentication
+
   def index
+
   end
 
   def create
+
   end
 
   def authentication
+
   end
 
-    private
-    def move_to_index
-      redirect_to action: :index
-      # indexアクションを強制的に実行する
-    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'article#index'
- get 'article', to: 'article#index'
 end
