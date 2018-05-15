fb92cb529d3be714cea33c8a48c476536a473b03
a <>
2018-05-15 09:49:13 +0900

    controller追加

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,15 +1,29 @@
 class ArticleController < ApplicationController
   def index
-
+  # @article = Article.all
   end
 
   def create
+  @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = '正常に投稿されました'
+      redirect_to @article
+    else
+      flash.now[:danger] = '投稿されませんでした'
+      render :new
+    end
+  end
 
+  def new
+  @article = Article.new
   end
 
   def authentication
 
   end
 
+def show
+    @article = Article.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root 'article#index'
+  resources :articles
 end
